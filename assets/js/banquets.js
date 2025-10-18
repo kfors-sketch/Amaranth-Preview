3ae40d2458f277fd86b3248c7eaf38d7473d3924
kfors-sketch <kfors@verizon.net>
2025-10-18 09:49:14 -0400

    Add files via upload

--- assets/js/banquets.js
+++ assets/js/banquets.js
@@ -2,16 +2,49 @@
 window.BANQUETS = [
   {
     id: "gala",
-    name: "Gala Banquet",
-    datetime: "2026-02-20 6:00 PM",
-    description: "Evening with keynote and awards.",
+    name: "Trails & Trteasures Feast",
+    datetime: "Saturday, April 18th at 5 PM",
+    description: "YADA YADA.",
     options: [
       { id:"adult", label:"Adult Ticket", price:60 },
       { id:"child", label:"Child Ticket", price:30 }
     ],
-    dietary: ["Vegetarian","Vegan","Gluten-free","Dairy-free","Nut allergy","Shellfish allergy"],
+    dietary: ["Chicken Entree","Beef Entree","Vegetarian Entree","?????"],
     active: true
   },
+  {
+    id: "Breakfast",
+    name: "Grand Floor Officers Breakfast",
+    datetime: "Sunday, April 19th at 9 AM",
+    description: "It is a plated Breakfast",
+    options: [
+      { id:"adult", label:"Adult Ticket", price:35 }
+    ],
+    dietary: ["Vegetarian","Gluten-free"],
+    active: true
+  }
+  {
+    id: "luncheon",
+    name: "Members Luncheon",
+    datetime: "2026-02-21 12:00 PM",
+    description: "",
+    options: [
+      { id:"adult", label:"Adult Ticket", price:35 }
+    ],
+    dietary: ["Vegetarian","Gluten-free"],
+    active: true
+  }
+  {
+    id: "luncheon",
+    name: "Members Luncheon",
+    datetime: "2026-02-21 12:00 PM",
+    description: "",
+    options: [
+      { id:"adult", label:"Adult Ticket", price:35 }
+    ],
+    dietary: ["Vegetarian","Gluten-free"],
+    active: true
+  }
   {
     id: "luncheon",
     name: "Members Luncheon",
