9c89e7cf33c9902bc06c1746dffc11f2ccfac12d
kfors-sketch <kfors@verizon.net>
2025-10-29 20:15:41 -0400

    Add files via upload

--- api/router.js
+++ api/router.js
@@ -559,4 +559,4 @@ export default async function handler(req, res) {
 }
 
 // 👇 Node 22 runtime hint for Vercel
-export const config = { runtime: "nodejs22.x" };
+export const config = { runtime: "nodejs" };
